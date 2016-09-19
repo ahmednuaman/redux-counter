adb8428d890336667fb2967d8ceb56769727e5c3
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-09-19 15:35:47 +0100

    sorting react

--- src/js/react/app.js
+++ src/js/react/app.js
@@ -1,16 +1,29 @@
+import autobind from 'autobind-decorator'
 import React from 'react'
 import { render } from 'react-dom'
 import store from 'reduxstore/store'
 import { decrement, increment } from 'reduxstore/action/index'
 
+@autobind
 class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
 
   componentDidMount() {
+    this.subscribeToStore()
+  }
+
+  subscribeToStore() {
     store.subscribe(() => {
       const state = store.getState()
 
       this.state.count = state.counter
-    }
+    })
   }
 
   increment() {
@@ -22,33 +35,20 @@ class Counter extends React.Component {
   }
 
   render() {
-    const count = this.state.count
     return (
       <div className="row">
         <div className="col-xs-8">
-          <input className="form-control input-lg" value="{count}" />
+          <input className="form-control input-lg" value={this.state.count} />
         </div>
         <div className="col-xs-4">
           <div className="btn-group">
-            <button className="btn btn-default btn-lg" onClick="{this.increment()}">+</button>
-            <button className="btn btn-default btn-lg" onClick="{this.decrement()}">-</button>
+            <button className="btn btn-default btn-lg" onClick={this.increment}>+</button>
+            <button className="btn btn-default btn-lg" onClick={this.decrement}>-</button>
           </div>
         </div>
       </div>
     )
   }
 }
-
-const Counter = React.createClass({
-  ,
-
-  increment: () => increment(),
-
-  decrement: () => decrement(),
-
-  render: () => (
-    
-  )
-})
 
 render(<Counter />, document.getElementById('app'))
