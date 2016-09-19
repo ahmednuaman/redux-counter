89826bd9f0c34c1eefad2e22b85bda969d184e48
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-09-19 14:38:24 +0100

    added vanilla

---
+++ src/js/react/app.js
@@ -0,0 +1,4 @@
+import react from 'react'
+import store from 'reduxstore/store'
+import { decrement, increment } from 'reduxstore/action/index'
+
---
+++ src/js/vanilla/app.js
@@ -0,0 +1,15 @@
+import store from 'reduxstore/store'
+import { decrement, increment } from 'reduxstore/action/index'
+
+const countEl = document.getElementById('count')
+const decrementEl = document.getElementById('decrement')
+const incrementEl = document.getElementById('increment')
+
+decrementEl.addEventListener('click', () => decrement())
+incrementEl.addEventListener('click', () => increment())
+
+store.subscribe(() => {
+  const state = store.getState()
+
+  countEl.value = state.counter
+})
